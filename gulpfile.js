6ee88f49093b59bc383b3769ad3d6f0b7ff94f3f
pragathik25 <pkshetty4446@gmail.com>
2023-07-04 13:41:34 +0530

    specbee

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const { src, dest, watch, series } = require('gulp')
+const sass = require('gulp-sass')(require('sass'));
+
+function buildStyles() {
+    return src('styles/*.scss')
+    .pipe(sass())
+    .pipe(dest('CSS'))
+}
+
+function watchTask() {
+    watch(['styles/*.scss'], buildStyles)
+}
+
+exports.default = series(buildStyles, watchTask)
