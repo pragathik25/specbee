996d7848bc6e5643a9bd5a58105c7806d529ce8b
pragathik25 <pkshetty4446@gmail.com>
2023-07-06 17:53:59 +0530

    specbee

--- spec.js
+++ spec.js
@@ -1,11 +1,13 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const header = document.querySelector(".header");
 const hamburgerInner = document.querySelector(".hamburger__inner");
 const navMenuSubMenu = document.querySelector(".nav-menu__sub-menu");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+    header.classList.toggle("active");
 })
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
